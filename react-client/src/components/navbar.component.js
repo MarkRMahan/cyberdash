1b838f74c892e6d7b3e279ace462effe76f9a134
Mark R Mahan <36490225+MarkRMahan@users.noreply.github.com>
2021-05-12 00:12:02 -0500

    stuff

--- react-client/src/components/navbar.component.js
+++ react-client/src/components/navbar.component.js
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <nav className="cybernav navbar navbar-expand navbar-dark bg-dark">
+    <nav className="cybernav navbar navbar-expand">
       <ul className="navbar-nav">
         <li className="nav-item">
           <a href="/weapons" className="navbar-brand nav-option">
@@ -21,7 +21,7 @@ const NavBar = () => {
       <div className="outer-nav-art rounded-top">
         <svg xmlns="http://www.w3.org/2000/svg" width="0.266667in" height="10.6667in" viewBox="0 0 24 960" preserveAspectRatio="xMinYMid slice">
           <path id="Imported Path"
-                fill="#343a40" stroke="black" strokeWidth="1"
+                fill="#181a1a" stroke="#65c5cc" strokeWidth="1"
                 d="M 24.00,952.00
                   C 24.00,952.00 24.00,800.00 24.00,800.00
                     24.00,800.00 24.00,792.00 24.00,792.00
