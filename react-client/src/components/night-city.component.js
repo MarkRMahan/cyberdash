c2a82355c6eb16496e7792ca30cb59042d4e7c06
Mark R Mahan <36490225+MarkRMahan@users.noreply.github.com>
2021-05-20 23:03:35 -0500

    Nearly finished with the Night City page

--- react-client/src/components/night-city.component.js
+++ react-client/src/components/night-city.component.js
@@ -225,44 +225,53 @@ export default class NightCityMap extends Component {
             </map>
           </div>
         </div>
-        <div className="nc-col container">
+        <div>
           <div className="row nc-paragraph">
             <div className="">
               <h1 id="nc-header">
                 Welcome to Night City <i>Punk</i>
               </h1>
               <p>
-                Neque convallis a cras semper auctor neque vitae tempus. Faucibus purus in massa tempor nec feugiat nisl. Velit scelerisque in dictum non. Ornare suspendisse sed nisi lacus. Rhoncus urna neque viverra justo nec. Mauris in aliquam sem fringilla ut morbi. Dui accumsan sit amet nulla facilisi. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Vulputate enim nulla aliquet porttitor lacus luctus. Neque vitae tempus quam pellentesque nec nam aliquam. Malesuada fames ac turpis egestas integer eget aliquet nibh. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Turpis massa sed elementum tempus egestas.
+                Night City, also known as the "City of Dreams", was established at the head of Del Coronado Bay, California in 1994 by a man with a vision, Richard Night. It was originally known as Coronado City long before it became the "city-state" that it is today.
+              </p>
+              <p>
+                Night City has a variety of different zones and buildings across the city. From its dense urban skyscrapers and crowded suburbs, to the lawless outskirts bordering the desert, this city is home to five million people in the year 2045.
+                This metropolis is considered a thrilling and exciting location to visit, as well as an interesting and vibrant place to live for those with the means to do so, and a hellscape to the city's disenfranchised who don't.
+              </p>
+              <p>
+                Here you can interact with a map of the city to get an idea of just what you're working with, or maybe what you're up against. Click on any of the Districts on the map to get a closer look at them. They don't contain any ICE. Promise.
               </p>
             </div>
           </div>
           <div className="row nc-paragraph">
             <p id="zoneDescription">
-              Think you got what it takes Choom?
+              Think you got what it takes Choom? Go ahead and select a Zone below to get a brief description of the different Zones found throughout Night City.
             </p>
           </div>
-          <div className="row nc-paragraph">
-            <div className="col-md-6 nc-col">
-              <ZoneType zoneColor="blue" zoneHeader="Executive Zone" zoneText="This is a special area of the City that has been cordoned off and gated for the use of High-Level Corporate executives and their families only."/>
+          <div className="">
+            <div className="row nc-paragraph zone-row">
+              <div className="col-md-6 nc-col">
+                <ZoneType zoneColor="blue" zoneHeader="Executive Zone" zoneText="This is a special area of the City that has been cordoned off and gated for the use of High-Level Corporate executives and their families only."/>
+              </div>
+              <div className="col-md-6">
+                <ZoneType zoneColor="green" zoneHeader="Rebuilding Urban Center" zoneText="These are parts of the city that escaped the worst of both the War and the bomb. In the Rebuilding Center, cranes and construction gear are everywhere, building new Corporate towers and crowded urban megabuildings."/>
+              </div>
             </div>
-            <div className="col-md-6">
-              <ZoneType zoneColor="green" zoneHeader="Rebuilding Urban Center" zoneText="These are parts of the city that escaped the worst of both the War and the bomb. In the Rebuilding Center, cranes and construction gear are everywhere, building new Corporate towers and crowded urban megabuildings."/>
+            <div className="row nc-paragraph zone-row">
+              <div className="col-md-6 nc-col">
+                <ZoneType zoneColor="orange" zoneHeader="Overpacked Suburbs" zoneText='Now filled with sprawling tent cities and unregulated refugee camps crammed with the displaced of the War, this is a place on the edge of disaster, where once safe and secure "Beavervilles" stood.'/>
+              </div>
+              <div className="col-md-6">
+                <ZoneType zoneColor="red" zoneHeader="Combat Zone" zoneText="The most dangerous and lawless part of the city which grew out of the original Combat Zone in the wake of the 4th Corporate War. The only justice here comes from the barrel of a gun."/>
+              </div>
             </div>
-          </div>
-          <div className="row nc-paragraph">
-            <div className="col-md-6 nc-col">
-              <ZoneType zoneColor="orange" zoneHeader="Overpacked Suburbs" zoneText='Now filled with sprawling tent cities and unregulated refugee camps crammed with the displaced of the War, this is a place on the edge of disaster, where once safe and secure "Beavervilles" stood.'/>
-            </div>
-            <div className="col-md-6">
-              <ZoneType zoneColor="red" zoneHeader="Combat Zone" zoneText="The most dangerous and lawless part of the city which grew out of the original Combat Zone in the wake of the 4th Corporate War. The only justice here comes from the barrel of a gun."/>
-            </div>
-          </div>
-          <div className="row nc-paragraph">
-            <div className="col-md-6 nc-col">
-              <ZoneType zoneColor="black" zoneHeader="Hot Zone" zoneText="The area most affected by the Night City Nuke, this is a blasted terrain full of wrecked, twisted skyscrapers, burned-out vehicles, and entombed bodies of the unlucky."/>
-            </div>
-            <div className="col-md-6">
-              <ZoneType zoneColor="white" zoneHeader="Outskirts &amp; Beyond" zoneText="The area just outside the City's sprawl is known as The Reclaimed Perimeter. It sits on the edge of lawlessness, save for the areas controlled by the Nomad Families. Beyond that is The Open Road, a highway populated only by people in transit or roving boostergangs on spiky ground cars and motorcycles."/>
+            <div className="row nc-paragraph zone-row">
+              <div className="col-md-6 nc-col">
+                <ZoneType zoneColor="black" zoneHeader="Hot Zone" zoneText="The area most affected by the Night City Nuke, this is a blasted terrain full of wrecked, twisted skyscrapers, burned-out vehicles, and entombed bodies of the unlucky."/>
+              </div>
+              <div className="col-md-6">
+                <ZoneType zoneColor="white" zoneHeader="Outskirts &amp; Beyond" zoneText="The area just outside the City's sprawl is known as The Reclaimed Perimeter. It sits on the edge of lawlessness, save for the areas controlled by the Nomad Families. Beyond that is The Open Road, a highway populated only by people in transit or roving boostergangs on spiky ground cars and motorcycles."/>
+              </div>
             </div>
           </div>
         </div>
