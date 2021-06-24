e149f11416dc95885ed581bfe639000e23b72654
Mark R Mahan <36490225+MarkRMahan@users.noreply.github.com>
2021-06-24 17:06:33 -0500

    Fixing up the ticket a bit

--- react-client/src/components/body-count-lottery.component.js
+++ react-client/src/components/body-count-lottery.component.js
@@ -71,30 +71,63 @@ export default class BodyCountLottery extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
+      <div className="container-fluid">
+        <div className="row title-row">
           <div className="col-md-6">
             Welcome to the Body Count Lotto! Today's lottery numbers are:
           </div>
         </div>
-        <div className="row lotto-ticket">
-          <div className="col-md-1 lotto-ticket-nums">
-            <span>{this.state.lotteryNumbers[0]}</span>
-          </div>
-          <div className="col-md-1 lotto-ticket-nums">
-            <span>{this.state.lotteryNumbers[1]}</span>
-          </div>
-          <div className="col-md-1 lotto-ticket-nums">
-            <span>{this.state.lotteryNumbers[2]}</span>
-          </div>
-          <div className="col-md-1 lotto-ticket-nums">
-            <span>{this.state.lotteryNumbers[3]}</span>
-          </div>
-          <div className="col-md-1 lotto-ticket-nums">
-            <span>{this.state.lotteryNumbers[4]}</span>
-          </div>
-          <div className="col-md-1 lotto-ticket-nums">
-            <span>{this.state.lotteryNumbers[5]}</span>
+        <div className="container-fluid lotto-ticket">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-12 lotto-info">
+                Ticket
+              </div>
+            </div>
+            <div className="container-fluid lotto-border">
+              <div className="row">
+                <div className="col-md-10">
+                  <div className="container-fluid">
+                    <div className="row">
+                      <div className="col-md-12">
+                        Testing
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-12" id="lotto-ticket-nums">
+                        <div className="col-md-1 lotto-ticket-num">
+                          <span>{this.state.lotteryNumbers[0]}</span>
+                        </div>
+                        <div className="col-md-1 lotto-ticket-num">
+                          <span>{this.state.lotteryNumbers[1]}</span>
+                        </div>
+                        <div className="col-md-1 lotto-ticket-num">
+                          <span>{this.state.lotteryNumbers[2]}</span>
+                        </div>
+                        <div className="col-md-1 lotto-ticket-num">
+                          <span>{this.state.lotteryNumbers[3]}</span>
+                        </div>
+                        <div className="col-md-1 lotto-ticket-num">
+                          <span>{this.state.lotteryNumbers[4]}</span>
+                        </div>
+                        <div className="col-md-1 lotto-ticket-num">
+                          <span>{this.state.lotteryNumbers[5]}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-2">
+                  <div className="container-fluid">
+                    <div className="row">
+                      <div className="col-md-12 lotto-ticket-num" id="total-body-count">
+                        <span>{this.state.totalBodyCount}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="row">
